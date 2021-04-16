f0967d8a722577a0dd953a38bb0517fc3a5f344e
Lex95 <andrea.lega1995@gmail.com>
2021-04-16 17:34:36 +0200

    aggiunta compilazione biglietto

--- myscript.js
+++ myscript.js
@@ -16,38 +16,43 @@ function onWindowLoad() {
         var kilometers = kmElement.value;
         var age = ageElement.value;
 
-        console.log(nome, kilometers, age);
-
-        document.getElementById("nomePasseggero").innerHTML = nome;
-        if (age == "minor") {
-            document.getElementById("etaPasseggero").innerHTML = "Sconto Minorenne";
-        } else if (age == "elder") {
-            document.getElementById("etaPasseggero").innerHTML = "Sconto Over 65";
+        if (nome == "" || kilometers == "") {
+            alert("Dati mancanti!");
+        } else if ((Number.isNaN(kilometers)) || (kilometers <= 0)) {
+            alert("Dati inseriti non validi! (Km non è un numero positivo)");
         } else {
-            document.getElementById("etaPasseggero").innerHTML = "Tariffa Normale";
+            document.getElementById("nomePasseggero").innerHTML = nome;
+            if (age == "minor") {
+                document.getElementById("etaPasseggero").innerHTML = "Sconto Minorenne";
+            } else if (age == "elder") {
+                document.getElementById("etaPasseggero").innerHTML = "Sconto Over 65";
+            } else {
+                document.getElementById("etaPasseggero").innerHTML = "Tariffa Normale";
+            }
+            
+            document.getElementById("costoBiglietto").innerHTML = costCalc(kilometers, age) + "€";
+
+            document.getElementById("carrozza").innerHTML = randomGenerator(2, 15);
+            document.getElementById("codiceCP").innerHTML = randomGenerator(90000, 100000);
         }
         
-        document.getElementById("costoBiglietto").innerHTML = costCalc(kilometers, age);
     })
 }
 
 function costCalc(kilometers, age) {
     var fee = (kilometers * 0.21);
-
-    if ((Number.isNaN(kilometers)) || (kilometers <= 0)) {
-        alert("Dati inseriti non validi! (Km non è un numero positivo)");
+    if (age == "minor") {
+        var cost = (fee * 0.8).toFixed(2);
+    }
+    else if (age == "elder") {
+        var cost = (fee * 0.6).toFixed(2);
     }
     else {
-        if (age == "minor") {
-            var cost = (fee * 0.8).toFixed(2);
-        }
-        else if (age == "elder") {
-            var cost = (fee * 0.6).toFixed(2);
-        }
-        else {
-            var cost = fee.toFixed(2)
-        }
-        return cost;
+        var cost = fee.toFixed(2)
     }
+    return cost;
 }
 
+function randomGenerator(num1, num2) {
+    return (Math.floor(Math.random() * (num2 - num1)) + num1);
+}
