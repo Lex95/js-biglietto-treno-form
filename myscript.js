c47dbf936e06147b53a000aff5735ab4b740d7ea
Lex95 <andrea.lega1995@gmail.com>
2021-04-16 16:59:53 +0200

    risoluzione problemi input

--- myscript.js
+++ myscript.js
@@ -9,38 +9,45 @@ function onWindowLoad() {
         event.preventDefault();
 
         var nomeElement = document.querySelector("[name='nome']");
-        console.log(nomeElement);
         var kmElement = document.querySelector("[name='km']");
-        console.log(kmElement);
-        var ageElement = document.querySelector("[name='etàPasseggero']");
-        console.log(ageElement);
+        var ageElement = document.querySelector("[name='eta']");
 
-        var form = event.currentTarget;
-        var formElements = form.elements;
-
-        var nome = formElements.nome.value;
-        var kilometers = formElements.kilometers.value;
-        var age = formElements.age.value;
+        var nome = nomeElement.value;
+        var kilometers = kmElement.value;
+        var age = ageElement.value;
 
         console.log(nome, kilometers, age);
+
+        document.getElementById("nomePasseggero").innerHTML = nome;
+        if (age == "minor") {
+            document.getElementById("etaPasseggero").innerHTML = "Sconto Minorenne";
+        } else if (age == "elder") {
+            document.getElementById("etaPasseggero").innerHTML = "Sconto Over 65";
+        } else {
+            document.getElementById("etaPasseggero").innerHTML = "Tariffa Normale";
+        }
+        
+        document.getElementById("costoBiglietto").innerHTML = costCalc(kilometers, age);
     })
 }
 
-// var fee = (kilometers * 0.21);
+function costCalc(kilometers, age) {
+    var fee = (kilometers * 0.21);
 
-// if ((Number.isNaN(kilometers) || Number.isNaN(age)) || (kilometers <= 0 || age <= 0)) {
-//     alert("Dati inseriti non validi!");
-//     document.getElementById("myId").innerHTML= "Impossibile calcolare la tariffa, ricaricare la pagina.";
-// }
-// else {
-//     if (age < 18) {
-//         var cost = (fee * 0.8).toFixed(2);
-//     }
-//     else if (age >= 65) {
-//         var cost = (fee * 0.6).toFixed(2);
-//     }
-//     else {
-//         var cost = fee.toFixed(2)
-//     }
-//     document.getElementById("myId").innerHTML= cost + " €";
-// }
+    if ((Number.isNaN(kilometers)) || (kilometers <= 0)) {
+        alert("Dati inseriti non validi! (Km non è un numero positivo)");
+    }
+    else {
+        if (age == "minor") {
+            var cost = (fee * 0.8).toFixed(2);
+        }
+        else if (age == "elder") {
+            var cost = (fee * 0.6).toFixed(2);
+        }
+        else {
+            var cost = fee.toFixed(2)
+        }
+        return cost;
+    }
+}
+
